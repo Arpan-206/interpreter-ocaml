06bd5fc4de379de678c601fe6efdf3c36876bdf8
Arpan Pandey <arpanpandey.aps@gmail.com>
2026-06-11 20:29:43 +0100

    codecrafters submit [skip ci]

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,35 @@
+type token = LEFT_PAREN | RIGHT_PAREN | EOF
+type lexer = { input : string; pos : int }
+
+let make input = { input; pos = 0 }
+
+let current { input; pos } =
+  if pos >= String.length input then '\x00' else input.[pos]
+
+let advance l = { l with pos = l.pos + 1 }
+
+let rec next_token l =
+  match current l with
+  | ' ' | '\t' | '\n' | '\r' -> next_token (advance l)
+  | '(' -> (advance l, LEFT_PAREN, "(")
+  | ')' -> (advance l, RIGHT_PAREN, ")")
+  | '\x00' -> (l, EOF, "")
+  | c ->
+      Printf.eprintf "[line 1] Error: Unexpected character: %c\n" c;
+      next_token (advance l)
+
+let token_to_string tok lexeme =
+  match tok with
+  | LEFT_PAREN -> Printf.sprintf "LEFT_PAREN %s null" lexeme
+  | RIGHT_PAREN -> Printf.sprintf "RIGHT_PAREN %s null" lexeme
+  | EOF -> "EOF  null"
+
+(* Returns true if a lex error occurred *)
+let rec scan l had_error =
+  let l', tok, lexeme = next_token l in
+  print_endline (token_to_string tok lexeme);
+  match tok with EOF -> had_error | _ -> scan l' had_error
+
 let () =
   if Array.length Sys.argv < 3 then (
     Printf.eprintf "Usage: ./your_program.sh tokenize <filename>\n";
@@ -12,14 +44,5 @@ let () =
 
   let file_contents = In_channel.with_open_text filename In_channel.input_all in
 
-  (* You can use print statements as follows for debugging, they'll be visible when running tests. *)
-  Printf.eprintf "Logs from your program will appear here!\n";
-
-  if String.length file_contents > 0 then
-    (* Implement & use your scanner here *)
-    failwith "Scanner not implemented"
-  else
-    (* TODO: Uncomment the code below to pass the first stage *)
-    print_endline "EOF  null";
-  (* Placeholder, replace this line when implementing the scanner *)
-  ()
+  let had_error = scan (make file_contents) false in
+  if had_error then exit 65
